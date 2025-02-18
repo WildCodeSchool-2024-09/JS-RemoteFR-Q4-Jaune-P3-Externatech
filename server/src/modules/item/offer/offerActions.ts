bc162b0e1bcf73648afdc725d2a914f93824b34e
Victoria Fonteny <victoriafonteny@live.fr>
2025-02-18 17:42:27 +0100

    offers with S and delet comment

--- server/src/modules/item/offer/offerActions.ts
+++ server/src/modules/item/offer/offerActions.ts
@@ -3,39 +3,31 @@ import offerRepository from "./offerRepository";
 
 const browse: RequestHandler = async (req, res, next) => {
   try {
-    // Fetch all categories
-    const offer = await offerRepository.readAll();
+    const offers = await offerRepository.readAll();
 
-    // Respond with the categories in JSON format
-    res.json(offer);
+    res.json(offers);
   } catch (err) {
-    // Pass any errors to the error-handling middleware
     next(err);
   }
 };
 
 const read: RequestHandler = async (req, res, next) => {
   try {
-    // Fetch a specific category based on the provided ID
     const offerId = Number(req.params.id);
     const offer = await offerRepository.read(offerId);
 
-    // If the category is not found, respond with HTTP 404 (Not Found)
-    // Otherwise, respond with the category in JSON format
     if (offer == null) {
       res.sendStatus(404);
     } else {
       res.json(offer);
     }
   } catch (err) {
-    // Pass any errors to the error-handling middleware
     next(err);
   }
 };
 
 const edit: RequestHandler = async (req, res, next) => {
   try {
-    // Update a specific category based on the provided ID
     const offer = {
       id: Number(req.params.id),
       title: req.body.title,
@@ -49,15 +41,12 @@ const edit: RequestHandler = async (req, res, next) => {
 
     const affectedRows = await offerRepository.update(offer);
 
-    // If the category is not found, respond with HTTP 404 (Not Found)
-    // Otherwise, respond with the category in JSON format
     if (affectedRows === 0) {
       res.sendStatus(404);
     } else {
       res.sendStatus(204);
     }
   } catch (err) {
-    // Pass any errors to the error-handling middleware
     next(err);
   }
 };
@@ -66,28 +55,22 @@ const add: RequestHandler = async (req, res, next) => {
   try {
     const newOffer = req.body;
 
-    // Create the offer
     const insertId = await offerRepository.create(newOffer);
 
-    // Respond with HTTP 201 (Created) and the ID of the newly inserted item
     res.status(201).json({ insertId });
   } catch (err) {
-    // Pass any errors to the error-handling middleware
     next(err);
   }
 };
 
 const destroy: RequestHandler = async (req, res, next) => {
   try {
-    // Delete a specific category based on the provided ID
     const offerId = Number(req.params.id);
 
     await offerRepository.delete(offerId);
 
-    // Respond with HTTP 204 (No Content) anyway
     res.sendStatus(204);
   } catch (err) {
-    // Pass any errors to the error-handling middleware
     next(err);
   }
 };
--- server/src/modules/item/offer/offerRepository.ts
+++ server/src/modules/item/offer/offerRepository.ts
@@ -14,7 +14,6 @@ type Offer = {
 
 class offerRepository {
   async create(offer: Omit<Offer, "id">) {
-    // Execute the SQL INSERT query to add a new category to the "category" table
     const {
       title,
       description,
@@ -37,15 +36,12 @@ class offerRepository {
       ],
     );
 
-    // Return the ID of the newly inserted item
     return result.insertId;
   }
 
   async readAll() {
-    // Execute the SQL SELECT query to retrieve all categories from the "category" table
     const [rows] = await DatabaseClient.query<Rows>("select * from offer");
 
-    // Return the array of categories
     return rows as Offer[];
   }
 
@@ -55,11 +51,10 @@ class offerRepository {
       [id],
     );
 
-    return rows.length > 0 ? rows[0] : null; // Retourne null si non trouvé
+    return rows.length > 0 ? rows[0] : null;
   }
 
   async update(offer: Offer) {
-    // Execute the SQL UPDATE query to update an existing category in the "category" table
     const [result] = await DatabaseClient.query<Result>(
       "update offer set title = ?, description = ?, date = ?, salary = ?, requirements = ?, entreprise_id = ?, contract_id = ? where id = ?",
       [
@@ -78,7 +73,6 @@ class offerRepository {
   }
 
   async delete(id: number) {
-    // Execute the SQL DELETE query to delete an existing offer from the "offer" table
     const [result] = await DatabaseClient.query<Result>(
       "delete from offer where id = ?",
       [id],
