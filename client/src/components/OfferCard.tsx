8a588788a650a879a5ce93f33a757cfc3f611fb1
Clarelle974 <clarelle.lemonnier@gmail.com>
2025-02-26 17:26:35 +0100

    Remove 1 div after review

--- client/src/components/OfferCard.tsx
+++ client/src/components/OfferCard.tsx
@@ -5,32 +5,30 @@ function OfferCard() {
   return (
     <article className="offer-card">
       <img src="/office-1.jpg" alt="équipe dans un bureau" />
-      <div>
-        <div className="header-card">
-          <img src="/logo-300- (1).png" alt="logo" />
-          <div className="remote">
-            <img src="/icon-home.png" alt="maison" />
-            <p>Télétravail</p>
-          </div>
+      <div className="header-card">
+        <img src="/logo-300- (1).png" alt="logo" />
+        <div className="remote">
+          <img src="/icon-home.png" alt="maison" />
+          <p>Télétravail</p>
         </div>
-        <div className="company-info">
-          <h2>Développeur Full Stack</h2>
-          <ul>
-            <li>
-              <strong>Tech Innov</strong>
-            </li>
-            <li>Paris</li>
-            <li>CDD</li>
-          </ul>
-        </div>
-        <div className="actions-buttons">
-          <Link to="/" className="colored-box">
-            VOIR L'OFFRE
-          </Link>
-          <Link to="/" className="light-box">
-            VOIR LES CANDIDATURES
-          </Link>
-        </div>
+      </div>
+      <div className="company-info">
+        <h2>Développeur Full Stack</h2>
+        <ul>
+          <li>
+            <strong>Tech Innov</strong>
+          </li>
+          <li>Paris</li>
+          <li>CDD</li>
+        </ul>
+      </div>
+      <div className="actions-buttons">
+        <Link to="/" className="colored-box">
+          VOIR L'OFFRE
+        </Link>
+        <Link to="/" className="light-box">
+          VOIR LES CANDIDATURES
+        </Link>
       </div>
     </article>
   );
