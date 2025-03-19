1f0037bcf0f2d747d499f1f1fd7e6b0ca7ced5bc
JULIAN HAZARD <hazardjulian74@gmail.com>
2025-03-19 11:59:29 +0100

    Pimpage menu burger

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -53,13 +53,10 @@ export default function NavBar() {
 
   const [isOpen, setIsOpen] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [activeLink, setActiveLink] = useState(String);
 
-  const burgerClose = () => {
-    return <p className="close">✖</p>;
-  };
-
-  const burgerOpen = () => {
-    return <p className="open">☰</p>;
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
   };
 
   const openModal = () => {
@@ -84,37 +81,73 @@ export default function NavBar() {
       <div className="burgerContainer">
         <button
           type="button"
-          onClick={() => setIsOpen(!isOpen)}
-          className="burgerButton"
+          onClick={toggleMenu}
+          className={`burgerButton ${isOpen ? "menuOpen" : ""}`}
         >
-          {isOpen ? burgerClose() : burgerOpen()}
+          {isOpen ? (
+            <span className="close">✖</span>
+          ) : (
+            <span className="open">☰</span>
+          )}
         </button>
-        {isOpen && (
-          <ul className="menuDroper">
-            {links
-              .filter((link) => link.role.includes(role))
-              .map((link) => (
-                <li key={link.name}>
-                  <Link to={link.path}>{link.name}</Link>
-                </li>
-              ))}
 
-            {role !== "anonymous" ? (
-              <button type="button" onClick={disconnect}>
-                Se déconnecter
+        <ul className={`menuDroper ${isOpen ? "open" : ""}`}>
+          {links
+            .filter((link) => link.role.includes(role))
+            .map((link) => (
+              <li key={link.name}>
+                <Link
+                  to={link.path}
+                  className={activeLink === link.name ? "active" : ""}
+                  onClick={() => {
+                    setActiveLink(link.name);
+                    setIsOpen(false);
+                  }}
+                >
+                  {link.name}
+                </Link>
+              </li>
+            ))}
+
+          {role !== "anonymous" ? (
+            <button
+              type="button"
+              className={activeLink === "disconnect" ? "active" : ""}
+              onClick={() => {
+                setActiveLink("disconnect");
+                disconnect();
+                setIsOpen(false);
+              }}
+            >
+              Se déconnecter
+            </button>
+          ) : (
+            <>
+              <button
+                type="button"
+                className={activeLink === "espace-entreprise" ? "active" : ""}
+                onClick={() => {
+                  setActiveLink("espace-entreprise");
+                  openModal();
+                  setIsOpen(false);
+                }}
+              >
+                Espace entreprise
               </button>
-            ) : (
-              <>
-                <button type="button" onClick={openModal}>
-                  Espace entreprise
-                </button>
-                <button type="button" onClick={openModal}>
-                  Se connecter
-                </button>
-              </>
-            )}
-          </ul>
-        )}
+              <button
+                type="button"
+                className={activeLink === "se-connecter" ? "active" : ""}
+                onClick={() => {
+                  setActiveLink("se-connecter");
+                  openModal();
+                  setIsOpen(false);
+                }}
+              >
+                Se connecter
+              </button>
+            </>
+          )}
+        </ul>
       </div>
 
       <ul className="menuDesktop">
