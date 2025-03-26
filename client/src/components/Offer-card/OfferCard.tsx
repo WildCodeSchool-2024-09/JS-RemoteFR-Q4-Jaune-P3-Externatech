37196a178f004f0be7de20ef0e3f296358acf7ef
Victoria Fonteny <victoriafonteny@live.fr>
2025-03-26 12:32:27 +0100

    delete things around register offer

--- client/src/components/Offer-card/OfferCard.tsx
+++ client/src/components/Offer-card/OfferCard.tsx
@@ -1,9 +1,7 @@
-import { useState } from "react";
 import { Link, useRevalidator } from "react-router-dom";
 import "./offer-card.css";
 import axios from "axios";
 import { useAuth } from "../../services/AuthContext";
-import Login from "../NavBar/Login";
 
 function OfferCard({ offer }: OfferDataProps) {
   const { role } = useAuth();
@@ -11,26 +9,6 @@ function OfferCard({ offer }: OfferDataProps) {
     "/companies/dashboard",
   );
   const { revalidate } = useRevalidator();
-
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const openModal = () => {
-    setIsModalOpen(!isModalOpen);
-    document.body.style.overflow = "hidden";
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-    document.body.style.overflow = "";
-  };
-  const [showModal, setShowModal] = useState(false);
-
-  const handleOpenModal = () => {
-    setShowModal(true);
-  };
-
-  const handleCloseModal = () => {
-    setShowModal(false);
-  };
 
   const deleteOffer = (id: number) => {
     if (window.confirm("Voulez-vous vraiment supprimer cette offre ?")) {
@@ -70,52 +48,6 @@ function OfferCard({ offer }: OfferDataProps) {
         </ul>
       </div>
       <div className="actions-buttons">
-        {role === "candidate" ? (
-          <>
-            <button
-              type="button"
-              className="register"
-              onClick={handleOpenModal}
-            >
-              <img
-                src="/Logos/Icon_bookmark.png"
-                alt="bookmark"
-                className="bookmark"
-              />
-            </button>
-          </>
-        ) : null}
-        {role === "anonymous" ? (
-          <>
-            <Login isOpen={isModalOpen} onClose={closeModal} />
-
-            <button type="button" className="register" onClick={openModal}>
-              <img
-                src="/Logos/Icon_bookmark.png"
-                alt="bookmark"
-                className="bookmark"
-              />
-            </button>
-          </>
-        ) : null}
-        {showModal && (
-          <section className="modal">
-            <section className="modal-content">
-              <h2>Succès</h2>
-              <p className="registered_p">Cette offre a été enregistrée !</p>
-              <Link to="/RegisteredOffers" className="registered_offers">
-                Voir mes offres enregistrées
-              </Link>
-              <button
-                type="button"
-                className="close_button"
-                onClick={handleCloseModal}
-              >
-                Fermer
-              </button>
-            </section>
-          </section>
-        )}
         <Link to={`/OfferDetails/${offer.id}`} className="light-box centered">
           VOIR L'OFFRE
         </Link>
